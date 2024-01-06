f97b3061db76a0f345d0833c44a247d0c5d10412
SAAMNETWORK <140245198+SAAMNETWORK@users.noreply.github.com>
2024-01-06 22:02:12 +0530

    Update extra.py

--- Adarsh/bot/plugins/extra.py
+++ Adarsh/bot/plugins/extra.py
@@ -17,12 +17,12 @@ async def maintainers(b,m):
     except Exception:
                 await b.send_message(
                     chat_id=m.chat.id,
-                    text="I am Made By [Rk_botz](https://t.me/Rk_botz)",
+                    text="I am Made By [SAAM Network](https://t.me/Itsme_Ahmed_Alisha)",
                     
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Owner💻", url=f"https://t.me/Rk_botowner")
+                                InlineKeyboardButton("Owner💻", url=f"https://t.me/Itsme_Ahmed_Alisha")
                             ]
                         ]
                     ),
@@ -42,7 +42,7 @@ async def follow_user(b,m):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("Subscribe ❤️", url=f"https://t.me/Rk_update")
+                                InlineKeyboardButton("Subscribe ❤️", url=f"https://t.me/SAAM_Network_2")
                             ]
                         ]
                     ),
