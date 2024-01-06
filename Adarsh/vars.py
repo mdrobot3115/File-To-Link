177c0f75662cfe7f32aa49375e8e810881db1ebf
SAAMNETWORK <140245198+SAAMNETWORK@users.noreply.github.com>
2024-01-06 13:05:06 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -25,7 +25,7 @@ class Var(object):
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'Itsme_Ahmed_Alisha'))
     if 'DYNO' in environ:
         ON_HEROKU = True
-        APP_NAME = str(getenv('Directvideos_bot'))
+        APP_NAME = str(getenv('https://Directvideos_bot'))
     
     else:
         ON_HEROKU = False
